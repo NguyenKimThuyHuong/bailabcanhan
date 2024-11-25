dfdf70456df219aef4e02aa95f361c9595e7f7ec
NguyenKimThuyHuong <thuyhuonga3s@gmail.com>
2024-11-25 22:54:47 +0700

    bài tập chương 5

---
+++ layout bài 5.4.js
@@ -0,0 +1,9 @@
+function showProductDetail() {
+    document.getElementById("product-list").style.display = "none";
+    document.getElementById("product-detail").style.display = "block";
+}
+
+function showProductList() {
+    document.getElementById("product-list").style.display = "flex";
+    document.getElementById("product-detail").style.display = "none";
+}
